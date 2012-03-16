7ff81e4b4ae1d7590b981259bf7a33e4d08187cc
David Hacker <davidhackerdvm@gmail.com>
2012-03-15 21:29:28 -0500

    remove vendor cm

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/motorola/solana/device_solana.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+#$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit some common CM stuff.
 #$(call inherit-product, vendor/cm/config/gsm.mk)
